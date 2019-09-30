bd8eed87f7dae3fe3d2a82d49c862f96b7ae47b0
Joe Schmitt <joe@compass.com>
2019-09-30 16:44:21 -0400

    Save pnpm into its own directory outside of node
    Fixes #2

--- lib/ensure_bin.sh
+++ lib/ensure_bin.sh
@@ -13,12 +13,14 @@ script_dir="$( cd -P "$( dirname "$source" )" >/dev/null 2>&1 && pwd )"
 source "$script_dir/colors.sh"
 source "$script_dir/config.sh"
 
-node_path="$HNVM_PATH/$node_ver"
+node_path="$HNVM_PATH/node/$node_ver"
 node_bin="$node_path/bin/node"
 npm_bin="$node_path/bin/npm"
 npx_bin="$node_path/bin/npx"
-pnpm_bin="$node_path/lib/node_modules/pnpm/bin/pnpm.js"
-pnpx_bin="$node_path/lib/node_modules/pnpm/bin/pnpx.js"
+
+pnpm_path="$HNVM_PATH/pnpm"
+pnpm_bin="$pnpm_path/lib/bin/pnpm.js"
+pnpx_bin="$pnpm_path/lib/bin/pnpx.js"
 
 function download_node() {
   platform=
@@ -34,7 +36,7 @@ function download_node() {
   rm -rf "${node_path}"
   mkdir -p "${node_path}"
 
-  blue "Downloading Node v$node_ver"> $COMMAND_OUTPUT
+  blue "Downloading node v$node_ver to $HNVM_PATH" > $COMMAND_OUTPUT
 
   if [[ "$HNVM_QUIET" == "true" ]]; then
     curl https://nodejs.org/dist/v${node_ver}/node-v${node_ver}-${platform}-x64.tar.gz --silent |
@@ -46,13 +48,14 @@ function download_node() {
 }
 
 function download_pnpm() {
-  blue "Downloading Pnpm v$pnpm_ver" > $COMMAND_OUTPUT
+  blue "Downloading pnpm v$pnpm_ver to $pnpm_path" > $COMMAND_OUTPUT
+
   if [[ "$HNVM_QUIET" == "true" ]]; then
     curl -L https://unpkg.com/@pnpm/self-installer --silent |
-      PNPM_VERSION=$pnpm_ver ${node_bin} > $COMMAND_OUTPUT
+      PNPM_VERSION=$pnpm_ver PNPM_DEST=$pnpm_path ${node_bin} > $COMMAND_OUTPUT
   else
     curl -L https://unpkg.com/@pnpm/self-installer |
-      PNPM_VERSION=$pnpm_ver ${node_bin} > $COMMAND_OUTPUT
+      PNPM_VERSION=$pnpm_ver PNPM_DEST=$pnpm_path ${node_bin} > $COMMAND_OUTPUT
   fi
 }
 
@@ -64,4 +67,4 @@ if [[ "$0" == *pnpm || "$0" == *pnpx ]] && [[ ! -x "$pnpm_bin" || "$(${node_bin}
   download_pnpm
 fi
 
-blue "Using hermetic NodeJS v$node_ver" > $COMMAND_OUTPUT
+blue "Using Hermetic NodeJS v$node_ver" > $COMMAND_OUTPUT
