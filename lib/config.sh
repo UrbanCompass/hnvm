149325229d5304076f900d774aa85bb049e64d82
Joe Schmitt <joe@compass.com>
2019-10-10 12:49:51 -0400

    Fix not actually finding git root .hnvmrc

--- lib/config.sh
+++ lib/config.sh
@@ -25,10 +25,10 @@ IFS=';' read -ra dirs_array <<< "$rc_dirs"
 for i in "${dirs_array[@]}"; do
   rc_file="$i/.hnvmrc"
 
-  if [[ $i == '.git' && -d "$PWD/.git" ]]; then
-    git_root="$(if [ "`which git && git rev-parse --show-cdup`" != "" ]; then cd `git rev-parse --show-cdup`; pwd; fi;)"
+  if [[ $i == '.git' && ! -z "$(which git)" ]]; then
+    git_root="$(if [ "`git rev-parse --show-cdup`" != "" ]; then cd `git rev-parse --show-cdup`; pwd; fi;)"
 
-    if [[ ! -z "$git_root" && -f "$git_root/.hnvmrc" ]]; then
+    if [[ ! -z "$git_root" && -f "$git_root/.hnvmrc" && ! -z "$(cat $git_root/.hnvmrc)" ]]; then
       rc_file="$git_root/.hnvmrc"
     fi
   fi
