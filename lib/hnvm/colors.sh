f07d92f883bcc66cd982d9fce04f38fe0ae924d0
Joe Schmitt <joe@compass.com>
2020-11-17 11:52:25 -0500

    Fix shellcheck in colors and config

--- lib/hnvm/colors.sh
+++ lib/hnvm/colors.sh
@@ -1,16 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
 # Print Coloring Functions
 function green {
-  echo $'\e[1;32m'"$@"$'\e[0m'
+  echo $'\e[1;32m'"$*"$'\e[0m'
 }
 
 function red {
-  echo $'\e[1;31m'"$@"$'\e[0m'
+  echo $'\e[1;31m'"$*"$'\e[0m'
 }
 
 function blue {
-  echo $'\e[1;34m'"$@"$'\e[0m'
+  echo $'\e[1;34m'"$*"$'\e[0m'
 }
 
 function yellow {
-  echo $'\e[1;33m'"$@"$'\e[0m'
+  echo $'\e[1;33m'"$*"$'\e[0m'
 }
