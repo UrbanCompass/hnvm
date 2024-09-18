2ca17f4f6667194f7e93727caa4a24a68ec88049
Joseph Galindo <joseph.galindo@compass.com>
2024-09-18 17:03:41 -0400

    add test just for tip-8901

--- test/node-subprocess.test.js
+++ test/node-subprocess.test.js
@@ -4,11 +4,12 @@ jest.setTimeout(60_000)
 
 describe('node with a fixed verison', () => {
   const VERSION = '16.13.0'
+  const PNPM_VERSION = '6.0.0'
   let context
 
   beforeAll(() => {
     context = createTestContext()
-    context.createPackageJson({engines: {node: VERSION}})
+    context.createPackageJson({engines: {node: VERSION, pnpm: PNPM_VERSION}})
   })
 
   afterAll(() => {
@@ -27,4 +28,17 @@ describe('node with a fixed verison', () => {
     )
     expect(hnvmProcess.stderr).toContain("WARNING: Further HNVM output will be redirected to '/dev/null'")
   })
+
+  it('[TIP-8901] should give only the pnpm version on stdout, when HNVM_OUTPUT_DESTINATION is a socket', () => {
+    const hnvmProcess = context.execFileSyncWithSocketOutput(
+      context.binaries.pnpm,
+      ['--version']
+    )
+
+    expect(hnvmProcess.stdout).toContain('6.0.0')
+    expect(hnvmProcess.stderr).toContain(
+      "WARNING: Could not find a writable, non-socket stdout redirect target!"
+    )
+    expect(hnvmProcess.stderr).toContain("WARNING: Further HNVM output will be redirected to '/dev/null'")
+  });
 })
